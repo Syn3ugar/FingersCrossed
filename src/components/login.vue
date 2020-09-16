ae0908492ac950254246a14e6cc2fffe75e877fa
waterkiller <2220918358@qq.com>
2020-09-16 20:08:58 +0800

    路由测试

---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>login test </h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from './components/login.vue'
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        {path:'/login',component:login}
+    ]
+})
+
+export default router
